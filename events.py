423bbc956790f89d170667bbd6a18f9efd81981f
TRVRNB <travis.renberg@gmail.com>
2025-11-10 00:04:20 -0800

    Add new events to INTRO_EVENTS and PHASE1
    Expanded the list of events for the first phase of the game.

--- events.py
+++ events.py
@@ -1,4 +1,6 @@
-INTRO_EVENTS = [
+# this is a list of random events or ones that are guaranteed to play. none of these have any prerequisites
+
+INTRO_EVENTS = [ # before phase 1
 	"stance_lavitia",
 	"stance_welfare",
 	"stance_minority",
@@ -6,7 +8,21 @@ INTRO_EVENTS = [
 	"stance_tax",
 ]
 
-PHASE1 = [
-
-
+PHASE1 = [ # first half of the game
+	"stance_political_assassination_otherparty",
+	"stance_political_assassination_playerparty",
+	"stance_lavitia_armed_rebellion",
+	"potential_election_interference",
+	"scandal_playerparty_members_bribed",
+	"scandal_tax_evasion",
+	"journalist_found_dead",
+	"stance_falling_literacy",
+	"stance_left_vs_right_violence",
+	"stance_national_defense_budget",
+	"stance_war_of_retribution",
+	"lavitia_offers_bribe",
+	"oligarchs_offer_bribe",
+	"key_member_retires",
+	"otherparty_commercial_bravado_playerparty",
+	"otherparty_asks_for_joint_message",
 ]
